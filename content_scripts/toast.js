3bc1c6eb725ba0342bb7809064c532c3ab974479
Daniel Rozenberg <me@danielrozenberg.com>
2020-04-06 23:20:06 -0400

    Toast's position fixed to `fixed` instead of `absolute`

--- content_scripts/toast.js
+++ content_scripts/toast.js
@@ -20,7 +20,7 @@ class Toast {
       `:host {
         display: block;
         margin-top: .5em;
-        position: absolute;
+        position: fixed;
         top: 0;
         width: 100%;
         z-index: 999999;
