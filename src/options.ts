14b55a8e519ab75499faf57757dd000d8ae03176
Daniel Rozenberg <me@danielrozenberg.com>
2023-11-27 19:03:02 -0500

    Show permission request in the options page, if not yet granted

--- src/options.ts
+++ src/options.ts
@@ -1,3 +1,4 @@
+import { ALL_HOST_PERMISSION } from './common/host-permission';
 import { localizeDocument } from './common/localization';
 import { type Options, getOptions } from './common/options';
 
@@ -6,6 +7,23 @@ import type { Browser } from 'webextension-polyfill';
 declare const browser: Browser;
 
 async function initialize() {
+  // Permissions should have been granted by the user on installation, but in
+  // case they weren't, un-hide the request here!
+  if (!(await browser.permissions.contains(ALL_HOST_PERMISSION))) {
+    const grantPermissionsContainer = document.getElementById(
+      'grantPermissionsContainer',
+    ) as HTMLDivElement;
+
+    grantPermissionsContainer.classList.remove('hidden');
+
+    document.getElementById('grant')?.addEventListener('click', async () => {
+      const granted = await browser.permissions.request(ALL_HOST_PERMISSION);
+      if (granted) {
+        grantPermissionsContainer.classList.add('hidden');
+      }
+    });
+  }
+
   const displayToastElement = document.getElementById(
     'displayToast',
   ) as HTMLInputElement;
